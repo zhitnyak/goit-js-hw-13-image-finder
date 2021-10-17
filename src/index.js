a39e6e78c36e18fd8c435562fe09c85ce4effc99
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-10-17 15:05:27 +0300

    add_axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import './sass/main.scss';
+import API from './js/apiService.js';
+import Notiflix from 'notiflix';
+import imgTpl from './templates/imgTpl.hbs';
 
+// Notiflix.Notify.success(`Hooray! We found ${res.totalHits} images.`);
+
+// Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+
+// Notiflix.Notify.success('Click Me', {
+//   timeout: 500,
+// });
+// Notiflix.Notify.warning('Click Me', {
+//   timeout: 500,
+// });
