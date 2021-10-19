a28449d38c0430f7fc769dc510b39bba4af8957d
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-10-19 23:33:40 +0300

    mistake

--- src/index.js
+++ src/index.js
@@ -29,8 +29,23 @@ function fetchImg(query, page = 1, perPage = 12) {
     .then(d => {
       console.log(d);
       createMarkup(d);
+      Notiflix.Notify.success(`Hooray! We found ${res.totalHits} images.`);
+
+      // делаем скролл вниз не мгновенно, с задержкой
+      setTimeout(
+        () =>
+          window.scrollTo({
+            // прокрутку делаем на всю высоту html вниз
+            top: document.documentElement.offsetHeight,
+            behavior: 'smooth',
+          }),
+        500,
+      );
     })
     .catch(error => console.log(error));
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.',
+  );
 }
 
 form.addEventListener('submit', getImg);
@@ -38,12 +53,34 @@ console.log(form);
 
 function getImg(e) {
   e.preventDefault();
+  imgBox.innerHTML = '';
+  // resetPage()
   const query = e.currentTarget.elements.query.value.trim();
   fetchImg(query);
-  // .then(createMarkup)
-  // .catch(error => console.log(error));
   form.reset();
 }
+
+loadMoreImgBtn.addEventListener('click', () => {
+  // page = 1;
+  fetchImg(imgBox);
+});
+
+// window.addEventListener('scroll', function () {
+//   //let block = document.getElementById('infinite-scroll');
+//   let counter = 1;
+//   // page = 1;
+//   fetchImg(imgBox);
+//   let contentHeight = imgBox.offsetHeight; // 1) высота блока контента вместе с границами
+//   let yOffset = window.pageYOffset; // 2) текущее положение скролбара
+//   let window_height = window.innerHeight; // 3) высота внутренней области окна документа
+//   let y = yOffset + window_height;
+
+//   // если пользователь достиг конца
+//   if (y >= contentHeight) {
+//     //загружаем новое содержимое в элемент
+//     imgBox.innerHTML = imgBox.innerHTML + fetchImg(perPage);
+//   }
+// });
 
 function createMarkup(data) {
   imgBox.insertAdjacentHTML('beforeend', imgTpl(data));
