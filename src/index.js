3f897545548bfc566588ab6cf392cc73e8a35f8f
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-10-24 12:53:59 +0300

    add_btn_back-to-top

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ApiImgService from './js/apiService.js';
 import Notiflix from 'notiflix';
 import imgTpl from './templates/imgTpl.hbs';
 import refs from './js/refs.js';
+import { foo } from './js/back-to-top.js';
+
+foo();
 
 const { form, loadMoreImgBtn, imgBox } = refs;
 const apiImgService = new ApiImgService();
@@ -13,6 +16,7 @@ loadMoreImgBtn.addEventListener('click', onLoadIMg);
 async function onGetImg(evt) {
   evt.preventDefault();
   imgBox.innerHTML = '';
+  loadMoreImgBtn.classList.add('hidden');
   apiImgService.resetPage();
   apiImgService.query = evt.currentTarget.elements.query.value.trim();
 
@@ -24,16 +28,18 @@ async function onGetImg(evt) {
     imgMarkup(result.hits);
 
     if (result.hits.length === 0) {
+      loadMoreImgBtn.classList.add('hidden');
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.',
       );
       return;
     }
     Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
+    loadMoreImgBtn.classList.remove('hidden');
   } catch (error) {
     console.log(error);
+    Notiflix.Notify.warning('Error!!!');
   }
-  // form.resetPage();
 }
 
 async function onLoadIMg() {
@@ -41,12 +47,15 @@ async function onLoadIMg() {
     const result = await apiImgService.fetchImg();
 
     if (imgBox.querySelectorAll('.photo-card').length === result.totalHits) {
-      console.log(result.totalHits);
+      loadMoreImgBtn.style.display = 'none';
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
     } else {
       imgMarkup(result.hits);
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.warning('Error!!!');
+  }
 }
 
 function imgMarkup(data) {
